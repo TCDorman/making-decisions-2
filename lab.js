d2e5cffd24d2a87f602f6baa393978c4da20be04
TCDorman <timothycolindorman@gmail.com>
2021-12-01 15:08:44 -0500

    Problem 7

--- lab.js
+++ lab.js
@@ -94,8 +94,9 @@ var brittanisBirthYear = 1986
   Create a for-loop that runs 10 times, and on each iteration logs the word 'hello'.
 */
 
-//CODE HERE
-
+// for(i = 0; i < 10; i++){
+//   console.log(`hello`)
+// }
 
 ////////// PROBLEM 6 //////////
 
@@ -103,16 +104,23 @@ var brittanisBirthYear = 1986
   Create a for-loop that runs 10 times, logging the numbers 1 through 10. Ex, on the first iteration it would log 1, on the second iteration it would log 2.
 */
 
-//CODE HERE
+//  for(let i = 1; i <= 10; i++){
+//    console.log(i)
+//  }
+
 
 
 ////////// PROBLEM 7 //////////
 
 /*
-  Create a for-loop that logs the numbers 0 through 10 backwards. Ex, on the first iteration it would log 10, on the second iteration it would log 9.
+  Create a for-loop that logs the numbers 0 through 10 backwards. Ex, on the first 
+  iteration it would log 10, on the second iteration it would log 9.
 */
 
-//CODE HERE
+// for(i=10; i >= 0; i--){
+//   console.log(i)
+// }
+
 
 // let score = 0
 // let passingScore = 7
